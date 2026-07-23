7f90fa3dcedd1a03cccc0039dcf912afaf8a38bc
MayraRuizT <aryam950812@gmail.com>
2026-07-23 11:34:52 +0200

    Untrack .ipynb_checkpoints from code folder and update .gitignore

--- code/.ipynb_checkpoints/03_get_singlets_raw_counts-checkpoint.r
+++
@@ -1,9 +0,0 @@
-## conda activate sceasy_env
-library(Seurat)
-library(Matrix)
-
-dat=readRDS('../data/PICseq/intermediate_steps/rds/scrna_phase_comparing_singlet_final.Rds')
-counts_matrix <- GetAssayData(object = dat, assay='RNA')
-writeMM(counts_matrix, '../data/PICseq/intermediate_steps/rds/singlets_raw/singlets_raw.mtx')
-write.csv(Features(dat), '../data/PICseq/intermediate_steps/rds/singlets_raw/singlets_raw_genes.csv')
-write.csv(dat@meta.data, '../data/PICseq/intermediate_steps/rds/singlets_raw/singlets_raw_meta.csv')
